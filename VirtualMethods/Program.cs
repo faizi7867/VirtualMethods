237ab4e230292e8f52b23a16df0a185e40d1c454
faisal sheikh <sheikhfaisal689@hotmail.com>
2021-11-19 12:36:29 +0530

    added class demo to the project

---
+++ VirtualMethods/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+        }
+    }
+    public class Demo
+    {
+
+    }
+}

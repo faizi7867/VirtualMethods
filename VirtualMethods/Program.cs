4256e392ae874ed679833f7ca4e25c1f6e64f2ad
faisal sheikh <sheikhfaisal689@hotmail.com>
2021-11-19 17:11:52 +0530

    replace array elements

--- VirtualMethods/Program.cs
+++ VirtualMethods/Program.cs
@@ -10,15 +10,36 @@ namespace VirtualMethods
     {
         static void Main(string[] args)
         {
-            int x = 10;
+
+            string s = "012345678";
+            char[] newarray = new char[s.Length];
+
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i % 2 == 0 && (s.Length-1)%2==0 && i== s.Length-1)
+                {
+                    newarray[i] = s[i];
+                }
+                else if(i % 2 == 0 && i != s.Length - 1)
+                {
+                    newarray[i] = s[i + 1];
+                }
+                else
+                {
+                    newarray[i] = s[i - 1];
+                }
+
+            }
+            
+         
+
+
+            Console.WriteLine(newarray);
+
+            Console.ReadLine();
+
         }
     }
-    public class Demo
-    {
 
-    }
-    public class Demo2
-    {
-
-    }
 }
